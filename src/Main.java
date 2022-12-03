29e8f805c1f7648fd15631fab25a366c992394d7
Luyba <lsorlova82@gmail.com>
2022-12-03 19:31:38 +0200

    method calculate added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 100_000;
-        int milePrice = 20;
-        int bonus = price / milePrice;
-        System.out.println("Your bonus:" + bonus);
-    }
-}
+            BonusMilesService service = new BonusMilesService();
+            int price = 10_000;
+            int miles = service.calculate(price);
+            System.out.println("Количество миль " + miles);
+        }
+                    }
